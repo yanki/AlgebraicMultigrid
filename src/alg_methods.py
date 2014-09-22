e394f9f9c84cd8a72ad9209e855a42b79e50bf5b
yanki <myankou@g.clemson.edu>
2014-09-22 19:43:45 -0400

    Updated the algorithm files.

--- src/alg_methods.py
+++ src/alg_methods.py
@@ -1,5 +1,4 @@
 import networkx as nx
-# import scipy as sci
 import settings
 
 Q = settings.threshhold
@@ -8,54 +7,43 @@ r = settings.neighborhood_size
 
 def AlgebraicMultigrid(G):
     """Accept a graph and attempt to solve it."""
-    # print nx.number_of_nodes(G)
-    # print nx.number_of_edges(G)
     if nx.number_of_nodes(G) <= 7:
         S = Solve(G)
     else:
-        G = Coarsen(G)
+        G = FutureVolume(G)
+        print G.nodes(data=True)
+        print G.edges(data=True)
+        """
+        1. find algebraic distance
+        2. future volume
+        3. coarse nodes
+        4. coarse edges
+        """
         # S = AlgebraicMultigrid(G)
         # S = Refine(S)
         S = None
     return S
 
 
-def Coarsen(G):
+def FutureVolume(G):
     """Coarsens the graph G."""
 
     global Q
     global r
 
     laplacian = nx.laplacian_matrix(G)
-    print laplacian
-    # print G.adj
-    volume_sum = 0
-    count = 0
-    for iteration, i in enumerate(nx.nodes_iter(G)):
+    for i in nx.nodes_iter(G):
         G.node[i]['future_volume'] = G.node[i]['volume']
         for j in nx.all_neighbors(G, i):
             degree = G.degree(j)
             adjacency = degree / min(r, Q * degree)
             sum_weight = 0.0
             for k in (k for k in nx.all_neighbors(G, i) if laplacian[i - 1, k - 1] < 0.0):
-                sum_weight = sum_weight + G.edge[i][k]['weight']
+                sum_weight += G.edge[i][k]['weight']
             norm_weight = G.edge[i][j]['weight'] / sum_weight
-            G.node[i]['future_volume'] = G.node[i]['future_volume'] + G.node[j]['volume'] * min(1.0, adjacency * norm_weight)
-        volume_sum = volume_sum + G.node[i]['future_volume']
-        count = iteration + 1
-    avg_volume = volume_sum / count
-    print "avg_volume: " + str(avg_volume) + " count: " + str(count)
-    print G.nodes(data=True)
-    print G.edges(data=True)
-    """
-    1. calculate future volume
-    2. algebraic distance between nodes
-    3. find coarse seeds for aggregates
-    4. find corase edges
-    5. filtering
-    """
-    print "Coarsening..."
-    return None
+            G.node[i]['future_volume'] += G.node[j]['volume'] * min(1.0, adjacency * norm_weight)
+    print "Calculated Future Volume."
+    return G
 
 
 def Solve(G):
